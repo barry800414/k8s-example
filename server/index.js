5dca71f9c77287370f4a574ded0ae95ba2c78b44
Wei Ming Chen <barry800414@gmail.com>
2021-04-03 21:52:49 +0800

    feat: update server

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const http = require('http');
 const os = require('os');
 
 console.log("server starting...");
+let count = 0;
 
 const handler = function(request, response) {
-  console.log("Received request from " + request.connection.remoteAddress); 
+  count += 1;
+  console.log("Received request from " + request.connection.remoteAddress + `  count:${count}`); 
   response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+  response.end("v1: You've hit " + os.hostname() + "\n");
 };
 const www = http.createServer(handler);
 www.listen(8080);
